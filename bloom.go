d1984859365ca4d9b91f12fb859046c954b4deda
Alex <alexjtyrrell@yahoo.co.uk>
2026-06-15 23:47:07 +0100

    K-hashes

--- bloom.go
+++ bloom.go
@@ -23,10 +23,30 @@ func NewSBF(m uint, k uint, h1 hashFunc, h2 hashFunc) *sbf {
 	}
 }
 
+func NewDefaultSBF(m uint, k uint) *sbf {
+	// Using standard lib FNV-1 and FNV-1a, non-cryptographic hash functions
+	return &sbf{
+		b: make([]uint64, (m+63)/64),
+		m: m,
+		k: k,
+		h1: func(data []byte) uint64 {
+			h := fnv.New64()
+        	h.Write(data)
+        	return h.Sum64()
+		},
+		h2: func(data []byte) uint64 {
+        	h := fnv.New64a()
+        	h.Write(data)
+        	return h.Sum64()
+    	},
+	}
+}
+
 func (sbf *sbf) Add([]byte) {
 	// hash the input k times
 
 	// set correct bits in b for the hashed input
+	// bucketValue |= uint64(1) << 27 
 }
 
 func (sbf *sbf) contains(data []byte) bool {
@@ -34,12 +54,20 @@ func (sbf *sbf) contains(data []byte) bool {
 	// true means probably not in the set
 	
 	// implement lookup
-	
+
 	return false
 }
 
-func hash(data []byte) uint64 {
-	h := fnv.New64()
-	h.Write(data)
-	return h.Sum64()
+func (sbf *sbf) initialHashPair(data []byte) (uint64, uint64) {
+    return sbf.h1(data), sbf.h2(data)
+}
+
+func (sbf *sbf) getKHashes(data []byte, k uint) []uint64 {
+    hash1, hash2 := sbf.initialHashPair(data) 
+    hashes := make([]uint64, k)
+    
+    for i := 0; i < int(k); i++ {
+        hashes[i] = hash1 + uint64(i)*hash2
+    }
+    return hashes
 }
